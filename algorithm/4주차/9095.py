70ce2d21ad7aba63117c853f0c010c166232e143
guk0 <abcgy39@naver.com>
2022-03-21 23:00:05 +0900

    change

--- algorithm/4주차/9095.py
+++ algorithm/4주차/9095.py
@@ -1,6 +1,9 @@
 # https://www.acmicpc.net/problem/9095
 # 1, 2, 3 더하기
+# dp
 
+
+# top down 방식
 from sys import stdin
 
 N = int(stdin.readline())
@@ -23,3 +26,23 @@ for _ in range(N):
   count = 0
   minus(x)
   print(count)
+
+
+# https://www.acmicpc.net/problem/15988
+# 1, 2, 3 더하기 3
+
+# bottom up 방식
+
+# from sys import stdin
+
+# T = int(stdin.readline())
+
+# dp = [0 for _ in range(1000000)]
+# dp[0], dp[1], dp[2] = 1, 2, 4
+
+# for i in range(3, 1000000):
+#   dp[i] = dp[i-1] % 1000000009 + dp[i-2] % 1000000009 + dp[i-3] % 1000000009
+
+# for _ in range(T):
+#   n = int(stdin.readline().strip())
+#   print(dp[n-1] % 1000000009)
